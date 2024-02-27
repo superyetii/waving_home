bda07311c2fac9ec5a2e42a8fbe20abdd23ea9bf
superyetii <nomad.storico@gmail.com>
2024-02-27 20:15:02 +0900

    added camera movement

---
+++ scenes/levels/Camera2D.gd
@@ -0,0 +1,33 @@
+extends Camera2D
+
+#Zoom Intensity
+var zoomIn = Vector2(1.1, 1.1)
+var zoomOut = Vector2(0.9, 0.9)
+#Camera Movement Speed
+var cameraMovementSpeed = 30
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	moveCamera()
+
+func _input(event):
+	if event.is_action_pressed("camera_scroll_in"):
+		zoom *= zoomIn
+	elif event.is_action_pressed("camera_scroll_out"):
+		zoom *= zoomOut
+
+func moveCamera():
+	if Input.is_action_pressed("camera_up"):
+		position.y -= cameraMovementSpeed
+	elif Input.is_action_pressed("camera_down"):
+		position.y += cameraMovementSpeed
+	elif Input.is_action_pressed("camera_left"):
+		position.x -= cameraMovementSpeed
+	elif Input.is_action_pressed("camera_right"):
+		position.x += cameraMovementSpeed
+	
